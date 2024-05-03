800dad75230fea53ac370e5847189a88f6723c5e
SEOKHWAN KIM <99132328+sokhan1@users.noreply.github.com>
2024-05-03 20:55:06 +0900

    Update Word alignment.py
    Word alignment.py

--- Word alignment.py
+++ Word alignment.py
@@ -1,6 +1,28 @@
 wordlist=[]
+lengthlist=[]
 trash=[]
 
+def LengthOrder(wordlist2,Number2):
+    wordlist2=[]
+    length1=0
+    wordlistbyorder=[]
+    ##test##
+    # len(wordlist2)
+    # len(wordlistbyorder)
+    ########
+    for i in range(0,Number2):
+        count = 0
+        length1=len(wordlist2[i])
+        for j in range(0,Number2):
+            length2=len(wordlist[j])
+            if length1>length2:
+                count+=1
+        wordlistbyorder.insert(count,wordlist[j])
+            
+        
+        
+        return wordlistbyorder
+    
 while True:
     Number = input('Enter the desired number')
     Number.replace(' ','')
@@ -13,9 +35,17 @@ while True:
         
 for i in range(0,Number):
     word=input('Enter word you want and the length of word must be less than or equal to 50')
+    word=word.lower()
     if len(word) <= 50:
         wordlist.append(word)
     else :
         trash.append(word)
-        
+
+wordlist=LengthOrder(wordlist2=wordlist,Number2=Number)
+print(wordlist)
+
+    
+       
 print(wordlist) # Check what strings are in wordlist
+
+
